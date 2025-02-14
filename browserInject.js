c561ce60ecdc6ae3bc85244e446b4a5a9c97f8ae
Ozan <ozankutlar@protonmail.com>
2025-02-14 12:31:02 +0300

    added stuff

--- browserInject.js
+++ browserInject.js
@@ -1,9 +1,11 @@
 function getStudents(){
 	let elementsMatrix = [];
 	
-	document.querySelectorAll('[id^="mod_assign_grading-"]').forEach(el => {
-		let match = el.id.match(/^mod_assign_grading-\d+_r(\d+)$/);
+	document.querySelectorAll('[id*="moduleTable-"]').forEach(el => {
+		// console.log("Checking " + el.id);
+		let match = el.id.match(/__item\d+-__xmlview\d+--moduleTable-(\d+)$/m);
 		if (match) {
+			// console.log("Matched " + match[1]);
 			let rowIndex = parseInt(match[1], 10);
 			elementsMatrix[rowIndex] = [];
 			
@@ -17,12 +19,9 @@ function getStudents(){
 }
 
 function processStudentMatrix(matrix) {
-	const relevantIndexes = [2, 3, 4, 8, 9];
+	const relevantIndexes = [3, 2, 5, 6];
 	
-	let filteredMatrix = matrix.filter(student => student[5].innerText.startsWith("Submitted for grading"));
-	// let filteredMatrix = matrix.filter(student => return true);
-	
-	let cleanedMatrix = filteredMatrix.map(student => relevantIndexes.map(index => student[index]));
+	let cleanedMatrix = matrix.map(student => relevantIndexes.map(index => student[index]));
 	
 	cleanedMatrix.forEach(student => {
 		cleanStudent(student);
@@ -33,20 +32,14 @@ function processStudentMatrix(matrix) {
 }
 
 function cleanStudent(arr) {
-	const link = arr[4].querySelector('a');
-	if (link) {
-		arr[4] = link.href;
-	} else {
-		console.log('No <a> tag found in the provided element.');
-	}
 	
-	// Name
+	// Ders Kodu
 	arr[0] = arr[0].innerText;
 	
-	// ID
+	// Section
 	arr[1] = arr[1].innerText;
 	
-	// Email
+	// Hoca
 	arr[2] = arr[2].innerText;
 
 	// Time
@@ -60,10 +53,9 @@ function downloadMatrixAsJson(matrix) {
 		if (Array.isArray(value) && value.length === 5) {
 			return {
 				name: value[0],
-				id: value[1],
-				email: value[2],
+				section: value[1],
+				teacher: value[2],
 				time: value[3],
-				link: value[4]
 			};
 		}
 		return value;
